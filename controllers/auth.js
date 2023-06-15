aaca6e86608dcc61573afd05cd23f822fc729aed
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-06-15 12:50:17 +0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -87,7 +87,7 @@ const logout = async (req, res, next) => {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, { token: "" });
     console.log(await User.findByIdAndUpdate(_id, { token: "" }));
-    res.json({
+    res.status(204).json({
       message: "Logout success",
     });
   } catch (error) {
